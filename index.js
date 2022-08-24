a809a0022cc7a6a18635174112899dd6bbcac214
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-08-24 07:22:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addingEventListener() {
     
     const input = document.getElementById('button');
     input.addEventListener('click', function() {
-      alert('Yeah here we are again');
+      alert('I bet you just clicked me');
 
     });
 }
